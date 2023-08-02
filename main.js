d951a57d084c853802919aa522a8f7d0ef165ea0
crislarr <116907840+Crislarr@users.noreply.github.com>
2023-08-02 02:07:45 +0000

    modify html and js files to include DOM compatible elements

--- main.js
+++ main.js
@@ -1,42 +1,54 @@
-function game() {
-  let computerScore = 0;
-  let playerScore = 0;
+// function game() {
+//   let computerScore = 0;
+//   let playerScore = 0;
 
-  for (i = 0; i < 5; i++) {
-    let roundResult = playRound();
+//   for (i = 0; i < 5; i++) {
+//     let roundResult = playRound();
 
-    if (roundResult === "invalid option") {
-      i--;
-    }
-    if (roundResult === "win") {
-      playerScore++;
-    }
-    if (roundResult === "lose") {
-      computerScore++;
-    }
-    if (computerScore === 3) {
-      return "You lost the game!";
-    }
-    if (playerScore === 3) {
-      return "You won the game!";
-    }
-  }
+//     if (roundResult === "invalid option") {
+//       i--;
+//     }
+//     if (roundResult === "win") {
+//       playerScore++;
+//     }
+//     if (roundResult === "lose") {
+//       computerScore++;
+//     }
+//     if (computerScore === 3) {
+//       return "You lost the game!";
+//     }
+//     if (playerScore === 3) {
+//       return "You won the game!";
+//     }
+//   }
 
-  if (computerScore > playerScore) {
-    return "You lost the game!";
-  } else if (playerScore > computerScore) {
-    return "You won the game!";
-  } else if (computerScore === playerScore) {
-    return "It's a tie after all!";
-  }
-}
+//   if (computerScore > playerScore) {
+//     return "You lost the game!";
+//   } else if (playerScore > computerScore) {
+//     return "You won the game!";
+//   } else if (computerScore === playerScore) {
+//     return "It's a tie after all!";
+//   }
+// }
 
-function playRound() {
-  let playerChoice = prompt("Rock, paper or scissors?").toLowerCase();
+const buttons = document.querySelectorAll(".button");
+
+const $rockButton = document.querySelector("#rock");
+const $paperButton = document.querySelector("#paper");
+const $scissorsButton = document.querySelector("#scissors");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playRound(button.id);
+  });
+});
+
+function playRound(playerChoosenButton) {
+  let playerChoice = playerChoosenButton;
   let computerChoice = getComputerChoice();
-  let gameResult = compareChoices(playerChoice, computerChoice);
-  console.log(chooseResultMessage(gameResult, playerChoice, computerChoice));
-  return gameResult;
+  let roundResult = compareChoices(playerChoice, computerChoice);
+  displayRoundResult(roundResult, playerChoice, computerChoice);
+  console.log(chooseResultMessage(roundResult, playerChoice, computerChoice));
 }
 
 function getComputerChoice() {
@@ -77,10 +89,10 @@ function compareChoices(playerChoice, computerChoice) {
 }
 
 function chooseResultMessage(gameResult, playerChoice, computerChoice) {
-  let winMessage = `You won! ${playerChoice} beats ${computerChoice}`;
-  let loseMessage = `You lost! ${computerChoice} beats ${playerChoice}`;
-  let tieMessage = "it's a tie!";
-  let invalidMessage = "Please choose a valid option";
+  const winMessage = `You won! ${playerChoice} beats ${computerChoice}`;
+  const loseMessage = `You lost! ${computerChoice} beats ${playerChoice}`;
+  const tieMessage = "it's a tie!";
+  const invalidMessage = "Please choose a valid option";
 
   if (gameResult === "win") {
     return winMessage;
@@ -93,4 +105,17 @@ function chooseResultMessage(gameResult, playerChoice, computerChoice) {
   }
 }
 
-console.log(game());
+function displayRoundResult(roundResult, playerChoice, computerChoice) {
+  const roundResultDiv = document.querySelector("#round-result");
+  roundResultDiv.innerHTML = "";
+
+  let resultMessage = document.createElement("p");
+  resultMessage.innerText = chooseResultMessage(
+    roundResult,
+    playerChoice,
+    computerChoice
+  );
+
+  roundResultDiv.appendChild(resultMessage);
+}
+// console.log(game());
